315872af64f62d1cb8bb2c74c6e7335d5db81f8b
Abdul basit <81569402+1103817@users.noreply.github.com>
2022-10-01 18:32:22 +0500

    Add files via upload

---
+++ algorithm.py
@@ -0,0 +1,15 @@
+def insertion_sort(arr):
+	for j in range(1, len(arr)):
+		key = arr[j]
+		i = j - 1
+
+		while i >= 0 and arr[i] > key:
+			arr[i+1] = arr[i]
+			i = i - 1
+		arr[i+1] = key
+
+
+myArr = [1, 23, 4, 5, 6, 2]
+
+insertion_sort(myArr)
+print(myArr)
